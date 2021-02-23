ce481c34b33442f7d0ef078285b61578febce98f
Sachin Verma <sachinverma53121@gmail.com>
2021-02-23 22:56:59 +0530

    setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const products = require("./products.json");
+const product_details = require("./product.json");
+
+//READ Request Handlers
+app.get("/", (req, res) => {
+  res.status(200).send({
+    response: "OK",
+    message: "Sachin E-Commerce Products API",
+    "api-routes": {
+      "all-products": "/api/products",
+      "single-product": "/api/product/:id",
+    },
+  });
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(200).send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = product_details.find((prod) => prod.id === req.params.id);
+
+  if (!product)
+    res.status(404).send({
+      response: "Not Fount",
+      message: "Product Not Found!",
+      "api-routes": {
+        "all-products": "/api/products",
+        "single-product": "/api/product/:id",
+      },
+    });
+  res.status(200).send(product);
+});
+
+//PORT ENVIRONMENT VARIABLE
+const port = process.env.PORT || 8080;
+app.listen(port, () =>
+  console.log(`Running E-Commerce API Server on ${port}..`)
+);
