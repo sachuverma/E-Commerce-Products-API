50c41d4241e6fc4e38e248a3d77afcf3b74d4650
Sachin Verma <sachinverma53121@gmail.com>
2021-02-23 23:48:46 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+var cors = require('cors')
+
+
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const products = require("./products.json");
 const product_details = require("./product.json");
